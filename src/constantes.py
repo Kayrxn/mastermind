54a7c08df9c52cf7a9017f17c4d60a4728ca1cfe
Kayrxn <xharukax99@gmail.com>
2025-12-11 09:21:50 +0100

    fix(constantes.py): Cambiar parámetros

--- src/constantes.py
+++ src/constantes.py
@@ -1,5 +1,5 @@
 colores = ["rojo", "verde", "azul", "amarillo", "blanco", "negro"]
 longitud_codigo = 4
-tamanio_poblacion = 25
-prob_mutacion = 0.25
+tamanio_poblacion = 50
+prob_mutacion = 0.10
 max_generaciones = 14
