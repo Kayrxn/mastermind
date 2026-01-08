5f0275767feee4c78f56311dfa340e7e67a94be2
francis85a <franperezcid85@gmail.com>
2026-01-08 09:57:40 +0100

    build: Eliminar src

--- src/constantes.py
+++
@@ -1,5 +0,0 @@
-COLORES = ["rojo", "verde", "azul", "amarillo", "blanco", "negro"]
-LONGITUD_CODIGO = 4
-TAMANIO_POBLACION = 80
-PROB_MUTACION = 0.20
-MAX_GENERACION = 14
--- src/cruce.py
+++
@@ -1,6 +0,0 @@
-def cruzar_padres(padre1, padre2):
-    #Paso 6: Cruza dos padres.
-
-    hijo1 = padre1[:2] + padre2[2:] 
-    hijo2 = padre2[:2] + padre1[2:]
-    return hijo1, hijo2
